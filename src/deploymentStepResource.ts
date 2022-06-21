e03eb54cf09982c0d2eae74febb18032db3cb48c
John Bristowe <john.bristowe@octopus.com>
2022-06-21 17:31:20 +1000

    feat: added NewDeploymentStepResource

--- src/deploymentStepResource.ts
+++ src/deploymentStepResource.ts
@@ -1,10 +1,19 @@
 import type { ActionProperties } from "./actionProperties";
 import type { DeploymentActionResource } from "./deploymentActionResource";
-import type { ResourceWithId } from ".";
+import type { NewNamedResource } from "./namedResource";
+import type { ResourceWithId } from "./resource";
 
 export interface DeploymentStepResource extends ResourceWithId {
   Id: string;
   Name: string;
+  Properties: ActionProperties;
+  Condition: RunCondition;
+  StartTrigger: StartTrigger;
+  PackageRequirement: PackageRequirement;
+  Actions: DeploymentActionResource[];
+}
+
+export interface NewDeploymentStepResource extends NewNamedResource {
   Properties: ActionProperties;
   Condition: RunCondition;
   StartTrigger: StartTrigger;
